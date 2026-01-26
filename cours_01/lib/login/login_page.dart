9174c676de039a31831888f4f5c2f7c90821aa06
Edouard Marquez <animatrix30@gmail.com>
2026-01-26 09:54:11 +0100

    Corrigé

--- cours_01/lib/login/login_page.dart
+++ cours_01/lib/login/login_page.dart
@@ -5,6 +5,55 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Log in or sign up',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EmailAddress extends StatelessWidget {
+  const EmailAddress({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ContinueButton extends StatelessWidget {
+  const ContinueButton({Key? key, this.onPressed}) : super(key: key);
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class OrDivider extends StatelessWidget {
+  const OrDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ContinueWithButton extends StatelessWidget {
+  const ContinueWithButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
